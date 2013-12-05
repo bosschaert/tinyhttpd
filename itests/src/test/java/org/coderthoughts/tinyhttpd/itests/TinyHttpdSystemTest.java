6d1bd856bf4bea405c95a0b003bfd739ddb7556c
David Bosschaert <david.bosschaert@gmail.com>
2013-12-05 19:31:49 +0000

    wip

--- itests/src/test/java/org/coderthoughts/tinyhttpd/itests/TinyHttpdSystemTest.java
+++ itests/src/test/java/org/coderthoughts/tinyhttpd/itests/TinyHttpdSystemTest.java
@@ -1,33 +1,25 @@
 package org.coderthoughts.tinyhttpd.itests;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 
-import javax.inject.Inject;
-
-import org.junit.Before;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.ops4j.pax.exam.Configuration;
 import org.ops4j.pax.exam.CoreOptions;
 import org.ops4j.pax.exam.Option;
 import org.ops4j.pax.exam.junit.PaxExam;
-import org.osgi.framework.Bundle;
-import org.osgi.framework.BundleContext;
 
-//@RunWith(JUnit4TestRunner.class)
 @RunWith(PaxExam.class)
 public class TinyHttpdSystemTest {
-    @Inject
-    private BundleContext ctx;
-
     @Configuration
     public Option[] config() {
         return CoreOptions.options(
-            // CoreOptions.equinox(),
+            // CoreOptions.vmOption("-Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5005"), // for debugging
             CoreOptions.vmOption("-Dfelix.fileinstall.dir=" + System.getProperty("user.dir")
                 + File.separator + "target" + File.separator + "test-classes" + File.separator + "load"),
-            // CoreOptions.vmOption("-Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5005"),
             CoreOptions.mavenBundle("org.apache.felix", "org.apache.felix.configadmin").versionAsInProject(),
             CoreOptions.mavenBundle("org.apache.felix", "org.apache.felix.fileinstall").versionAsInProject(),
             CoreOptions.mavenBundle("io.netty", "netty-buffer").versionAsInProject(),
@@ -36,40 +28,27 @@ public class TinyHttpdSystemTest {
             CoreOptions.mavenBundle("io.netty", "netty-common").versionAsInProject(),
             CoreOptions.mavenBundle("io.netty", "netty-handler").versionAsInProject(),
             CoreOptions.mavenBundle("io.netty", "netty-transport").versionAsInProject(),
-            CoreOptions.mavenBundle("org.coderthoughts.tinyhttpd", "core").versionAsInProject()
-            );
-//        ,
-//            CoreOptions.junitBundles());
-    }
-
-    @Before
-    public void configureFileInstall() {
-//        System.setProperty("felix.fileinstall.dir", System.getProperty("user.dir") +
-//                File.separator + "target" + File.separator + "test-classes");
+            CoreOptions.mavenBundle("org.coderthoughts.tinyhttpd", "core").versionAsInProject(),
+            CoreOptions.junitBundles());
     }
 
 	@Test
-	public void testFoo() throws Exception {
-	    System.out.println("%%% " + System.getProperty("felix.fileinstall.dir"));
-
-		for (Bundle b : ctx.getBundles()) {
-            if (b.getBundleId() == 0) {
-                System.out.println("### " + b + " - " + b.getVersion());
-            }
-		    if (b.getSymbolicName().contains("felix")) {
-		        System.out.println("### " + b + " - " + b.getVersion());
-		    }
-            if (b.getSymbolicName().contains("netty")) {
-                System.out.println("### " + b + " - " + b.getVersion());
-            }
-            if (b.getSymbolicName().contains("coderthoughts")) {
-                System.out.println("### " + b + " - " + b.getVersion());
-            }
-		}
-
-		System.out.println("$$$ " + System.getProperty("user.dir"));
-		Thread.sleep(1000000);
-		String content = new String(Streams.suck(new URL("http://localhost:7070").openStream()));
-		System.out.println("Loaded: " + content);
+	public void testReadWebResource() throws Exception {
+		String content = tryReadURL("http://localhost:7070");
+		Assert.assertTrue(content.contains("Allaert Joachim David Bosschaert"));
 	}
+
+    private String tryReadURL(String url) throws Exception {
+        int retries = 20;
+        while (--retries > 0) {
+            try {
+                System.out.println("Trying to read from: " + url);
+                return new String(Streams.suck(new URL(url).openStream()));
+            } catch (Exception e) {
+                // ignore
+            }
+            Thread.sleep(500);
+        }
+        throw new IOException("Unable to read from URL: " + url);
+    }
 }
