2ceeb535ae8a6f2560786979647050d61e384bb8
David Bosschaert <david.bosschaert@gmail.com>
2013-12-06 10:24:09 +0000

    wip

--- core/src/test/java/org/coderthoughts/tinyhttpd/DirectoryRendererTest.java
+++ core/src/test/java/org/coderthoughts/tinyhttpd/DirectoryRendererTest.java
@@ -11,11 +11,14 @@ public class DirectoryRendererTest {
         File dir1 = new File(getClass().getResource("/dir1").getFile());
         String html = DirectoryRenderer.renderDirectoryHTML("/dir1/", dir1);
 
-        int fileIdx = html.indexOf("<a href=\"/dir1/blah.txt\">blah.txt</a>");
+        int parentDirIdx = html.indexOf("<a href=\"/dir1/..\">parent directory</a>");
         int subDirIdx = html.indexOf("<a href=\"/dir1/subdir\">subdir</a>");
+        int fileIdx = html.indexOf("<a href=\"/dir1/blah.txt\">blah.txt</a>");
 
+        Assert.assertTrue(parentDirIdx > 0);
         Assert.assertTrue(fileIdx > 0);
         Assert.assertTrue(subDirIdx > 0);
+        Assert.assertTrue("Parent directory should be listed before other directories", parentDirIdx < subDirIdx);
         Assert.assertTrue("Subdir should be listed before the file", subDirIdx < fileIdx);
     }
 }
