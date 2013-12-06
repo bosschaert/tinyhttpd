1f598fc8acbca6867f286afc0d43cef93cb21d10
David Bosschaert <david.bosschaert@gmail.com>
2013-12-06 09:48:39 +0000

    wip

--- core/src/main/java/org/coderthoughts/tinyhttpd/DirectoryRenderer.java
+++ core/src/main/java/org/coderthoughts/tinyhttpd/DirectoryRenderer.java
@@ -1,6 +1,7 @@
 package org.coderthoughts.tinyhttpd;
 
 import java.io.File;
+import java.util.Comparator;
 
 class DirectoryRenderer {
     private DirectoryRenderer() {
@@ -10,10 +11,56 @@ class DirectoryRenderer {
     static String renderDirectoryHTML(String uri, File directory) {
         StringBuilder sb = new StringBuilder();
 
-        sb.append("<!DOCTYPE html><html><head><title>Directory: " + uri);
-        sb.append("</title></head>");
-        sb.append("</html>\n");
+        sb.append("<!DOCTYPE html><html><head><title>Directory: ");
+        sb.append(uri);
+        sb.append("</title></head><body>");
+        sb.append("<h1>Directory: ");
+        sb.append(uri);
+        sb.append("</h1>");
+        sb.append("<table>");
+
+        File[] files = directory.listFiles();
+        // Arrays.sort(files, new SortingFileComparator());
+        renderFiles(sb, files);
+
+        sb.append("</table></body></html>\n");
 
         return sb.toString();
+    }
+
+    private static void renderFiles(StringBuilder sb, File[] files) {
+        for (File f : files) {
+            sb.append("<tr><td>");
+            sb.append(f.getName());
+            sb.append("</td><td>");
+            if (f.isDirectory()) {
+                sb.append("directory");
+            } else {
+                sb.append(f.length());
+                sb.append(" bytes");
+            }
+            sb.append("</td></tr>");
+        }
+    }
+
+    private static class SortingFileComparator implements Comparator<File> {
+        @Override
+        public int compare(File f1, File f2) {
+            if (f1.isDirectory()) {
+                if (f2.isDirectory()) {
+                    return f1.getName().compareTo(f2.getName());
+                } else {
+                    // Directories go before ordinary files
+                    return -1;
+                }
+            } else {
+                if (f2.isDirectory()) {
+                    // Ordinary files go after directories
+                    return 1;
+                }
+            }
+
+            return f1.getName().compareTo(f2.getName());
+        }
     }
 }
--- core/src/test/java/org/coderthoughts/tinyhttpd/DirectoryRendererTest.java
+++ core/src/test/java/org/coderthoughts/tinyhttpd/DirectoryRendererTest.java
@@ -1,10 +1,16 @@
 package org.coderthoughts.tinyhttpd;
 
+import java.io.File;
+
+import org.junit.Assert;
 import org.junit.Test;
 
 public class DirectoryRendererTest {
     @Test
     public void testDirectoryRendering() {
-
+        File dir1 = new File(getClass().getResource("/dir1").getFile());
+        String html = DirectoryRenderer.renderDirectoryHTML("/dir1/", dir1);
+        Assert.assertTrue(html.contains("blah.txt"));
+        Assert.assertTrue(html.contains("subdir"));
     }
 }
